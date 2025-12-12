f5c2e54ca34b331ee4d7101858a41534a0394021
Jhon Perez <juanp20077@gmail.com>
2025-12-12 17:57:21 -0600

    clean up motor group

--- packages/atum/src/hardware/motor_group.rs
+++ packages/atum/src/hardware/motor_group.rs
@@ -1,8 +1,9 @@
+use std::time::Duration;
+
 use vexide::prelude::Motor;
 
-use crate::controllers::pid::Pid;
-
 use super::average;
+use crate::controllers::pid::Pid;
 
 pub struct MotorGroup {
     motors: Vec<Motor>,
@@ -11,7 +12,10 @@ pub struct MotorGroup {
 
 impl MotorGroup {
     pub fn new(motors: Vec<Motor>, motor_controller: Option<MotorController>) -> Self {
-        Self { motors, motor_controller }
+        Self {
+            motors,
+            motor_controller,
+        }
     }
 
     pub fn set_voltage(&mut self, voltage: f64) {
@@ -20,10 +24,10 @@ impl MotorGroup {
         }
     }
 
-    pub fn set_velocity(&mut self, velocity: i32) { 
+    pub fn set_velocity(&mut self, velocity: i32) {
         for motor in self.motors.iter_mut() {
             match self.motor_controller {
-                Some(ref mut controller) => {
+                Some(mut controller) => {
                     let motor_velocity = motor.velocity().unwrap_or_default();
                     let voltage = controller.output(velocity as f64, motor_velocity, None);
                     _ = motor.set_voltage(voltage);
@@ -56,13 +60,10 @@ impl MotorGroup {
 
         average(velocities) // * ratio
     }
-    
-    
 
     pub fn iter_mut(&mut self) -> core::slice::IterMut<'_, Motor> {
         self.motors.iter_mut()
     }
-
 }
 
 #[derive(Clone, Copy)]
@@ -73,23 +74,18 @@ pub struct MotorController {
     ka: f64,
 }
 
-impl MotorController{
+impl MotorController {
     pub fn new(pid: Pid, ks: f64, kv: f64, ka: f64) -> Self {
-        Self {
-            pid,
-            ks,
-            kv,
-            ka,
-        }
+        Self { pid, ks, kv, ka }
     }
 
     pub fn output(&mut self, target_rpm: f64, actual_rpm: f64, acceleration: Option<f64>) -> f64 {
         let error = target_rpm - actual_rpm;
-        
+
         let ff = self.kv + self.ks + acceleration.unwrap_or_default() * self.ka;
         // change duration to a non-const using Instant
         let pid = self.pid.output(error, Duration::from_millis(10));
-        
+
         ff + pid
     }
 }
