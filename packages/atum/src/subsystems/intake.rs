1096b79dc2bb8a3cc4351ebbfd309f7776b4a415
Jhon Perez <juanp20077@gmail.com>
2026-01-31 04:44:47 -0600

    add color sort
    Intake can now spit out balls if it is of the wrong color. Can now
    differentiate between alliance color using Color enum. RobotSettings to
    easily pass around configuration of robot.

--- packages/atum/src/subsystems/intake.rs
+++ packages/atum/src/subsystems/intake.rs
@@ -1,8 +1,12 @@
 use std::{cell::RefCell, rc::Rc, time::Duration};
 
 use vexide::{
-    prelude::Motor, task::{spawn, Task}, time::sleep
+    adi::digital::LogicLevel, prelude::{AdiDigitalOut, Motor, OpticalSensor}, task::{spawn, Task}, time::sleep
 };
+
+use crate::subsystems::Color;
+
+use super::RobotSettings;
 
 pub struct Intake {
     voltage: Rc<RefCell<f64>>,
@@ -10,7 +14,14 @@ pub struct Intake {
 }
 
 impl Intake {
-    pub fn new(mut top: Motor, mut bottom: Motor) -> Self {
+    pub fn new(
+        mut top: Motor,
+        mut bottom: Motor,
+        mut door: AdiDigitalOut,
+        color_sort: OpticalSensor,
+        delay: Duration,
+        settings: Rc<RefCell<RobotSettings>>,
+    ) -> Self {
         let voltage = Rc::new(RefCell::new(0.0));
 
         Self {
@@ -18,9 +29,36 @@ impl Intake {
             _task: spawn(async move {
                 loop {
                     let voltage = *voltage.borrow();
+                    let settings = *settings.borrow();
 
                     _ = top.set_voltage(voltage);
                     _ = bottom.set_voltage(voltage);
+
+                    if settings.enable_color {
+                        // Red hue -> 0-60
+                        // Blue hue -> 120-240
+                        let (alliance, opposing) = match settings.color {
+                            Color::Red => (0.0..60.0, 120.0..240.0),
+                            Color::Blue => (120.0..240.0, 0.0..60.0),
+                        };
+
+                        let hue = color_sort.hue().unwrap_or_default();
+                        let proximity = color_sort.proximity().unwrap_or_default();
+
+                        // switch low and high if piston starts differently
+                        let level = if alliance.contains(&hue) && proximity == 1.0 {
+                            Some(LogicLevel::Low)
+                        } else if opposing.contains(&hue) && proximity == 1.0 {
+                            Some(LogicLevel::High)
+                        } else {
+                            None
+                        };
+
+                        if let Some(level) = level {
+                            sleep(delay).await;
+                            _ = door.set_level(level);
+                        }
+                    }
 
                     sleep(Duration::from_millis(10)).await;
                 }
@@ -28,7 +66,9 @@ impl Intake {
         }
     }
 
-    pub fn set_command(&self, voltage: f64) -> f64 {
+    pub fn set_voltage(&self, voltage: f64) -> f64 {
         self.voltage.replace(voltage)
     }
+
+    pub fn test_door(&mut self) {}
 }
--- packages/atum/src/subsystems/mod.rs
+++ packages/atum/src/subsystems/mod.rs
@@ -1,2 +1,14 @@
 pub mod drivetrain;
 pub mod intake;
+
+#[derive(Clone, Copy)]
+pub struct RobotSettings {
+    color: Color,
+    enable_color: bool,
+}
+
+#[derive(Clone, Copy)]
+pub enum Color {
+    Red,
+    Blue,
+}
